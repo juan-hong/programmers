6e886e1b49ad917c876928943ac74f5d8175497a
jaun-hong <ghdwndks123@naver.com>
2026-08-29 23:06:22 +0900

    29.08.2026

---
+++ level1/12912.py
@@ -0,0 +1,13 @@
+"""문제 설명
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을
+리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
+"""
+
+def solution(a, b):
+    answer = 0
+    min_num = min(a,b)
+    max_num = max(a,b)
+    for x in range(min_num, max_num + 1):
+        answer += x
+    return answer
